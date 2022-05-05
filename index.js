6e68c5985530db5ebb801f45cac95d18fe4ffd2b
Ay0cheese <94671327+Ay0cheese@users.noreply.github.com>
2022-05-05 16:54:01 -0700

    got into google voice
    now I need to create a new message and send a message

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ puppeteer.launch({ headless: false }).then(async browser => {
     const page = await browser.newPage()
     await page.setViewport({ width: 800, height: 600 })
 
-    //await page.goto('https://www.surfline.com/surf-report/the-wedge/5842041f4e65fad6a770882b')
-    //await page.waitForTimeout(1000)
   
-    await page.goto('https://accounts.google.com/signin/v2/identifier?service=grandcentral&passive=1209600&osid=1&continue=https%3A%2F%2Fvoice.google.com%2Fsignup&followup=https%3A%2F%2Fvoice.google.com%2Fsignup&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-    await page.waitForSelector('.Xb9hP')
-    await page.click('.Xb9hP')
-    await page.waitForSelector('.Xb9hP')
-    await page.type('.whsOnd.zHQkBf, wadegoodman95@gmail.com')
-    await browser.close()
+    await page.goto('https://accounts.google.com/signin/v2/identifier')
+    await page.type('[type="email"]', "wadegoodman95@gmail.com")
+    
+    await page.click('#identifierNext')
+    await page.waitForTimeout(1500)
+    await page.type('[type="password"]', "Cheeseandbananas")
+    await page.waitForTimeout(1500)
+    await page.waitForSelector('.whsOnd.zHQkBf')
+    await page.click('.VfPpkd-vQzf8d')
+    await page.waitForSelector('.whsOnd.zHQkBf')
+    await page.click('.VfPpkd-vQzf8d')
+    await page.goto('https://voice.google.com/u/0/messages')
+    await page.waitForSelector('.signUpLink')
+    await page.click('.signUpLink')
+    await page.goto('https://voice.google.com/u/0/messages')
+    await page.click('[id="apiproxy794f59e83560ea5d6299b7ff3b7a1d7788beabed0.1220890444"]');
+    
+    
+    
+    //await browser.close()
+
+
   })
   
