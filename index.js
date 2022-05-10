b2a0048ea5ebccdd81471fc6bce28ca67cc1e475
Ay0cheese <94671327+Ay0cheese@users.noreply.github.com>
2022-05-10 09:30:13 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -27,34 +27,34 @@ const puppeteer = require('puppeteer')
   })
 })()
 
-//This is the google voice section:
+// This is the google voice section:
 
-// puppeteer.launch({ headless: false }).then(async browser => {
-//     const page = await browser.newPage()
-//     await page.setViewport({ width: 800, height: 600 })
-//     await page.goto('https://accounts.google.com/signin/v2/identifier')
-//     await page.type('[type="email"]', "wadegoodman95@gmail.com")
+puppeteer.launch({ headless: false }).then(async browser => {
+    const page = await browser.newPage()
+    await page.setViewport({ width: 800, height: 600 })
+    await page.goto('https://accounts.google.com/signin/v2/identifier')
+    await page.type('[type="email"]', "wadegoodman95@gmail.com")
     
-//     await page.click('#identifierNext')
-//     await page.waitForTimeout(1500)
-//     await page.type('[type="password"]', "Cheeseandbananas")
-//     await page.waitForTimeout(1500)
-//     await page.waitForSelector('.whsOnd.zHQkBf')
-//     await page.click('.VfPpkd-vQzf8d')
-//     await page.waitForSelector('.whsOnd.zHQkBf')
-//     await page.click('.VfPpkd-vQzf8d')
-//     await page.goto('https://voice.google.com/u/0/messages')
-//     await page.waitForSelector('.signUpLink')
-//     await page.click('.signUpLink')
-//     await page.goto('https://voice.google.com/u/0/messages')
-//     await page.click('[id="apiproxy794f59e83560ea5d6299b7ff3b7a1d7788beabed0.1220890444"]');
+    await page.click('#identifierNext')
+    await page.waitForTimeout(1500)
+    await page.type('[type="password"]', "Cheeseandbananas")
+    await page.waitForTimeout(1500)
+    await page.waitForSelector('.whsOnd.zHQkBf')
+    await page.click('.VfPpkd-vQzf8d')
+    await page.waitForSelector('.whsOnd.zHQkBf')
+    await page.click('.VfPpkd-vQzf8d')
+    await page.goto('https://voice.google.com/u/0/messages')
+    await page.waitForSelector('.signUpLink')
+    await page.click('.signUpLink')
+    await page.goto('https://voice.google.com/u/0/messages')
+    await page.click('[id="apiproxy794f59e83560ea5d6299b7ff3b7a1d7788beabed0.1220890444"]');
     
     
     
-    //await browser.close()
+    await browser.close()
 
 
-  // })
+  })
   
 
 
